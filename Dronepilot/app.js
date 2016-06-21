73970e65e5fac0168a2a053250b9970da3d348d8
René <rene.brueggemann@fh-muenster.de>
2016-06-21 12:07:45 +0200

    behandlung der start

--- Dronepilot/app.js
+++ Dronepilot/app.js
@@ -8,7 +8,17 @@ var config = require('./config.js');
 var request = require('request');
 var atob = require('atob');
 var fileIO = require("fs");
-var ftpClient = require('ftp-client');
+var ftpClient = require('ftp-client'),
+    config = {
+        host: '192.168.42.1',
+        port: 21,
+        user: 'anonymous',
+        password: 'anonymous@'
+    },
+    options = {
+        logging: 'basic'
+    },
+    client = new ftpClient(config, options);
 
 var bebop = require('node-bebop')
 var drone = bebop.createClient();
@@ -53,7 +63,7 @@ if (fakedata == true) {
 
 drone.connect(function() {
 
-    drone.MediaStreaming.videoEnable(1);
+    //drone.MediaStreaming.videoEnable(1);
     //Signal on Batterychange
     drone.on('battery', function(data) {
         console.log("DEBUG sending battery: "+data+" %...");
@@ -138,6 +148,28 @@ io.on('connection', function(socket) {
     });
 
     socket.on('disconnect', function() {});
+
+    socket.on('start-button', function (data) {
+        
+        for(var i=0; i<fieldData.length; i++){
+            var job = fieldData[i]
+            if(job.id == data){
+                break;
+            }
+        }
+        
+        //var mavparser = new MavParser(job.coordinates,'flightplan',1);
+        //mavparser.start();
+        
+        client.connect(function () {
+            client.upload(['../mavlink/flightplan.mavlink'], '/internal_000/flightplans', {
+                baseDir: '../mavlink',
+                overwrite: 'all'
+            }, function (result) {
+                console.log(result);
+            });
+        });
+    });
 });
 
 function sendState() {
